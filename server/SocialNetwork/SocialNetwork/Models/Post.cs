9b87d2fbdb9eaa408856e70226a7387e8039526f
cristycr <139634450+cristycr@users.noreply.github.com>
2026-01-20 18:22:57 +0100

    Add Post model and EF Core packages
    Introduced the Post model to represent user posts. Added Entity Framework Core SQLite and Tools packages to enable database integration and migrations.

---
+++ server/SocialNetwork/SocialNetwork/Models/Post.cs
@@ -0,0 +1,10 @@
+﻿namespace SocialNetwork.Models;
+public class Post {
+    public long Id { get; set; }
+    public required long UserId { get; set; }
+    public required User User { get; set; }
+    public required DateTime CreationDate { get; set; } = DateTime.Now; // DateTime.UtcNow?
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Picture { get; set; } = string.Empty; // ?? Es de tipo string
+}
