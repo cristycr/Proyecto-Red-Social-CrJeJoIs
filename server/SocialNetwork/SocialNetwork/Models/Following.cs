625fe737a0002b2b1406fd596351d91afe55f961
IRios79 <1979irr@gmail.com>
2026-01-20 20:11:55 +0100

    Update Following.cs

--- server/SocialNetwork/SocialNetwork/Models/Following.cs
+++ server/SocialNetwork/SocialNetwork/Models/Following.cs
@@ -2,5 +2,10 @@
 {
     public class Following
     {
+        public long Id { get; set; }
+        public required long Follower { get; set; }
+        public required User UserFollower { get; set; }
+        public required long Followed { get; set; }
+        public required User UserFollowed { get; set; }
     }
 }
