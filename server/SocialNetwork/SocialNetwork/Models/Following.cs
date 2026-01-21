87cb08a6d6a017c1c4b97c0fa9d73c77e3febbd6
IRios79 <1979irr@gmail.com>
2026-01-21 08:29:55 +0100

    Update Following.cs

--- server/SocialNetwork/SocialNetwork/Models/Following.cs
+++ server/SocialNetwork/SocialNetwork/Models/Following.cs
@@ -1,11 +1,21 @@
-﻿namespace SocialNetwork.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace SocialNetwork.Models
 {
     public class Following
     {
         public long Id { get; set; }
+        // --- RELACIÓN 1: El que sigue ---
         public required long Follower { get; set; }
-        public required User UserFollower { get; set; }
+
+        [ForeignKey("Follower")] // Le decimos: "El objeto de abajo se llena usando la clave de arriba 'Follower'"
+        public required User UserFollower { get; set; } // Objeto de navegación
+
+        // --- RELACIÓN 2: El seguido ---
         public required long Followed { get; set; }
-        public required User UserFollowed { get; set; }
+
+        [ForeignKey("Followed")] 
+        public required User UserFollowed { get; set; } 
     }
 }
