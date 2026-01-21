9c9fdcf607ef0f7f0c0bead7c75fca078b7b1543
Jor <jgonjim2511@g.educaand.es>
2026-01-21 20:20:00 +0100

    Añadir Configuración DbContext SQLite a Program.cs
    Configurado "SocialNetworkContext" para usar SQLite con inyección de dependencias. Asegurada creación de base de datos en el inicio y resgitrado DbContext como "scoped"  para adecuada gestión de recursos.

--- server/SocialNetwork/SocialNetwork/Program.cs
+++ server/SocialNetwork/SocialNetwork/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using SocialNetwork.Models;
 
 namespace SocialNetwork;
 
@@ -10,10 +12,31 @@ public class Program
         // Add services to the container.
 
         builder.Services.AddControllers();
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+        // Configuración del DbContext para usar SQLite
+        builder.Services.AddDbContext<SocialNetworkContext>(options =>
+        {
+            // Aquí indicamos SQLite y la ruta de la base de datos
+            string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            options.UseSqlite($"DataSource={baseDir}socialnetwork.db");
+        });
+
         builder.Services.AddOpenApi();
 
+        // Añadimos el DbContext al servicio de inyección de dependencias
+        // Tiene que ser scoped para que cierre la conexión y limpie
+        // los recursos tras cada petición
+        builder.Services.AddScoped<SocialNetworkContext>();
+
         var app = builder.Build();
+
+        // Creamos un scope y nos aseguramos de que se crea la base de datos según
+        // tengamos configurado nuestro DbContext
+        using (IServiceScope scope = app.Services.CreateScope())
+        {
+            SocialNetworkContext dbContext = scope.ServiceProvider.GetRequiredService<SocialNetworkContext>();
+            dbContext.Database.EnsureCreated();
+        }
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
