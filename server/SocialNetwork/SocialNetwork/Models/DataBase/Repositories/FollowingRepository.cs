45f8e603df63bb1171bb4cfce7b120eed4d4ba1e
IRios79 <1979irr@gmail.com>
2026-01-23 23:09:56 +0100

    Update FollowingRepository.cs

--- server/SocialNetwork/SocialNetwork/Models/DataBase/Repositories/FollowingRepository.cs
+++ server/SocialNetwork/SocialNetwork/Models/DataBase/Repositories/FollowingRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;            //Herramientas de windows
+using SocialNetwork.Models.Database;
 using SocialNetwork.Models.Database.Entities;   //Los ingredientes de la BD
 using System.Threading.Tasks;                   //Otras herramientas: async, away...
 
@@ -7,7 +8,15 @@ namespace SocialNetwork.Models.DataBase.Repositories
 {
     public class FollowingRepository
     {
+        //Variable para guardar la conexion con la BD (¿¿CAMBIAR NOMBRE??)
+        private readonly SocialNetworkContext _context;
 
+        //CONSTRUCTOR. Se ejecuta cuando se crear el repositorio
+        //Recibe el context de Program y lo guarda en la variable
+        public FollowingRepository(SocialNetworkContext context)
+        {
+            _context = context;
+        }
 
 
     }
