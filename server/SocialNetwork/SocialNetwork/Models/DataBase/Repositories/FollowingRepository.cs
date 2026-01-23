fc6c847c9fc815fd6ce0cc7acec945d803ba4e9c
IRios79 <1979irr@gmail.com>
2026-01-23 22:44:14 +0100

    Create FollowingRepository.cs

---
+++ server/SocialNetwork/SocialNetwork/Models/DataBase/Repositories/FollowingRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;            //Herramientas de windows
+using SocialNetwork.Models.Database.Entities;   //Los ingredientes de la BD
+using System.Threading.Tasks;                   //Otras herramientas: async, away...
+
+
+namespace SocialNetwork.Models.DataBase.Repositories
+{
+    public class FollowingRepository
+    {
+
+
+
+    }
+}
