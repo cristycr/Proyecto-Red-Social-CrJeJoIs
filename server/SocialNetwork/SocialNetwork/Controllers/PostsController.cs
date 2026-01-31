8afd8bd900a7b7636fded19ea3fdb3be8b513622
cristycr <139634450+cristycr@users.noreply.github.com>
2026-01-31 21:11:41 +0100

    Updated PostController to use UnitOfWork

--- server/SocialNetwork/SocialNetwork/Controllers/PostsController.cs
+++ server/SocialNetwork/SocialNetwork/Controllers/PostsController.cs
@@ -12,43 +12,43 @@ namespace SocialNetwork.Controllers;
 public class PostsController : ControllerBase {
 
     // Inyección del Repositorio
-    private readonly PostRepository _postRepository;
-    public PostsController(PostRepository postRepository) {
-        _postRepository = postRepository;
+    private readonly UnitOfWork _unitOfWork;
+    public PostsController(UnitOfWork unitOfWork) {
+        _unitOfWork = unitOfWork;
     }
 
     // GET: api/posts
     [HttpGet]
     public async Task<IEnumerable<Post>> GetAllPosts() {
-        return await _postRepository.GetPostsAsync();
+        return await _unitOfWork.PostRepository.GetPostsAsync();
     }
 
     // GET: api/posts/by-user/5
     [HttpGet("by-user/{userId:long}")]
     public async Task<IEnumerable<Post>> GetPostsByUserId(long userId) {
-        return await _postRepository.GetPostsByUserIdAsync(userId);
+        return await _unitOfWork.PostRepository.GetPostsByUserIdAsync(userId);
     }
 
     // GET: api/posts/5
     [HttpGet("{id}")]
     public async Task<Post?> GetPostById(long id) {
-        return await _postRepository.GetPostByIdAsync(id);
+        return await _unitOfWork.PostRepository.GetPostByIdAsync(id);
     }
 
     [HttpPost]
     // Los parámetros son la Entidad (Post) y un objeto nuevo (post) que se crea
     // apartir del JSON que devuelve la petición POST
     public async Task<bool> AddPost([FromBody] Post post) {
-        return await _postRepository.AddPostAsync(post);
+        return await _unitOfWork.PostRepository.AddPostAsync(post);
     }
     
     [HttpPut]
     public async Task<bool> UpdatePost([FromBody] Post newPost) {
-        return await _postRepository.UpdatePostAsync(newPost);
+        return await _unitOfWork.PostRepository.UpdatePostAsync(newPost);
     }
 
     [HttpDelete]
     public async Task<bool> DeletePost([FromBody] Post post) {
-        return await _postRepository.DeletePostAsync(post);
+        return await _unitOfWork.PostRepository.DeletePostAsync(post);
     }
 }
