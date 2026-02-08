1e84b498c881268fa7b86986e699a1293f01c774
Jesus Felipe Fuentes Trigueros <fuentestriguerosjesus@gmail.com>
2026-02-08 12:26:15 +0100

    PasswordHelper
    ya esta el metodo que hashea la contraseña, falta aplicarlo en el controlador o donde haga falta que no lo tengo muy claro

---
+++ server/SocialNetwork/SocialNetwork/Helpers/PasswordHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace SocialNetwork.Helpers {
+    internal class PasswordHelper {
+        public static string Hash(string password) {
+            byte[] inputBytes = Encoding.UTF8.GetBytes(password);
+            byte[] inputHash = SHA256.HashData(inputBytes);
+            return Encoding.UTF8.GetString(inputHash);
+        }
+    }
+}
