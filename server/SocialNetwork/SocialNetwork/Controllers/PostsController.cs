846995cf00c22c75fc03ba4f62ca73164e0ae32f
Jesus Felipe Fuentes Trigueros <fuentestriguerosjesus@gmail.com>
2026-02-16 20:11:13 +0100

    Add login posts endpoint; drop PicturePath
    Add an authorized GET endpoint (api/posts/login) in PostsController: GetAllPostsOrderByLogin(long userId) which calls _unitOfWork.PostRepository.GetPostsByCreationDateLoginAsync and maps Post entities to GetPostDto. Also add the Authorization using directive and remove the PicturePath property from GetPostDto to reflect the updated response shape.

--- server/SocialNetwork/SocialNetwork/Controllers/PostsController.cs
+++ server/SocialNetwork/SocialNetwork/Controllers/PostsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SocialNetwork.Models.Database;
 using SocialNetwork.Models.Database.Entities;
 using SocialNetwork.Models.Dtos.Posts;
@@ -19,6 +20,24 @@ public class PostsController : ControllerBase {
     [HttpGet]
     public async Task<IEnumerable<GetPostDto>> GetAllPostsOrderBy() {
         IEnumerable<Post> posts = await _unitOfWork.PostRepository.GetPostsByCreationDateAsync();
+
+        IEnumerable<GetPostDto> postsDto = posts.Select(post =>
+        new GetPostDto() {
+            Id = post.Id,
+            UserId = post.UserId,
+            CreationDate = post.CreationDate,
+            Title = post.Title,
+            Description = post.Description
+        });
+
+        return postsDto;
+    }
+
+    // GET: api/posts
+    [Authorize]
+    [HttpGet("login")]
+    public async Task<IEnumerable<GetPostDto>> GetAllPostsOrderByLogin(long userId) {
+        IEnumerable<Post> posts = await _unitOfWork.PostRepository.GetPostsByCreationDateLoginAsync(userId);
 
         IEnumerable<GetPostDto> postsDto = posts.Select(post =>
         new GetPostDto() {
