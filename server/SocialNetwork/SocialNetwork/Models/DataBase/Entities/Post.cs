8ac49d8ea06bd9957d591938963e1a692afcface
Jesus Felipe Fuentes Trigueros <fuentestriguerosjesus@gmail.com>
2026-02-16 14:31:17 +0100

    post.c
    correcion de required

--- server/SocialNetwork/SocialNetwork/Models/DataBase/Entities/Post.cs
+++ server/SocialNetwork/SocialNetwork/Models/DataBase/Entities/Post.cs
@@ -1,12 +1,12 @@
 ﻿namespace SocialNetwork.Models.Database.Entities;
 public class Post {
     public long Id { get; set; }
-    public required long UserId { get; set; }
     // La fecha de creación se inicializa con la fecha y hora actual en UTC
     // Si se utiliza DateTime.Now, la hora se guarda en función de la zona horaria del servidor
     public DateTime CreationDate { get; set; } = DateTime.UtcNow;
     public string? Title { get; set; }
     public string? Description { get; set; }
     // Establece la relación con la entidad User
-    public required User User { get; set; }
+    public long UserId { get; set; }
+    public User? User { get; set; }
 }
