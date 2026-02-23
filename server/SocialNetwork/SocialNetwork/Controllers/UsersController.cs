c8a912e3bedf84fb1cc9e20ea2a0525289f7303c
Jesus Felipe Fuentes Trigueros <fuentestriguerosjesus@gmail.com>
2026-02-23 11:54:16 +0100

    controlador id/profile
    He creado un metodo en el userRepository que cuenta el numero de seguidos y seguidores de un usuario. Luego he hecho un dto con los campos necesarios para el perfil donde se usa esos metodos para mostrar un campo calculado del numero de seguidos y seguidores

--- server/SocialNetwork/SocialNetwork/Controllers/UsersController.cs
+++ server/SocialNetwork/SocialNetwork/Controllers/UsersController.cs
@@ -43,6 +43,24 @@ public class UsersController : ControllerBase {
         return getUsersDto;
     }
 
+    // metodo para obtener un usuario por su id, para mostrar su perfil
+    [HttpGet("{id}/profile")]
+    public async Task<GetUserProfileDto> GetUserById(long id) {
+        User? user = await _unitOfWork.UserRepository.GetByIdAsync(id);
+        if (user == null) {
+            throw new Exception("User not found");
+        }
+        GetUserProfileDto getUserCountDto = new GetUserProfileDto {
+            Id = user.Id,
+            Nickname = user.Nickname,
+            AvatarPath = user.AvatarPath!,
+            Biography = user.Biography,
+            FollowerCount = _unitOfWork.UserRepository.GetFollowerUsersCountAsync(id).Result,
+            FollowedCount = _unitOfWork.UserRepository.GetFollowedUsersCountAsync(id).Result,
+        };
+        return getUserCountDto;
+    }
+
     [HttpGet("Followers")]
     public async Task<IEnumerable<GetUserDto>> GetFollowerUsers(long userId) {
         // long userId = long.Parse(User.FindFirst("id").Value);
