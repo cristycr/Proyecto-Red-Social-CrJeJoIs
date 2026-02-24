982f7edaa657bbbd82c19fb669775f5d1ba6772e
Jor <jgonjim2511@g.educaand.es>
2026-02-24 15:42:55 +0100

    Crear IFileService

---
+++ server/SocialNetwork/SocialNetwork/Services/IFileService.cs
@@ -0,0 +1,8 @@
+﻿namespace SocialNetwork.Services
+{
+    public interface IFileService
+    {
+        Task<(string FileName, string Url)> SaveFileAsync(IFormFile file);
+        Task DeleteFileAsync(string fileName);
+    }
+}
