205dcd5095606d2e8d46c0367fecb5ef1d79d185
Jor <jgonjim2511@g.educaand.es>
2026-03-03 10:45:33 +0100

    Quitar SaveChangesAsync de BaseRepository

--- server/SocialNetwork/SocialNetwork/Models/DataBase/Repositories/BaseRepository.cs
+++ server/SocialNetwork/SocialNetwork/Models/DataBase/Repositories/BaseRepository.cs
@@ -23,11 +23,10 @@ public abstract class BaseRepository<TEntity, TId> : IRepository<TEntity, TId> w
         EntityEntry<TEntity> entry = await _dbContext.Set<TEntity>().AddAsync(entity);
         return entry.Entity;
     }
-    public async Task<TEntity> UpdateAsync(TEntity entity)
+    public Task<TEntity> UpdateAsync(TEntity entity)
     {
         _dbContext.Set<TEntity>().Update(entity);
-        await _dbContext.SaveChangesAsync();
-        return entity;
+        return Task.FromResult(entity);
     }
     public async Task DeleteAsync(TEntity entity) {
         _dbContext.Set<TEntity>().Remove(entity);
