3b3b14c1fc2031add2e8bf367646fa7ecd48d37d
Jor <jgonjim2511@g.educaand.es>
2026-03-06 10:19:05 +0100

    Renombrado a admin-guard.ts

--- client/social-network/src/app/guards/admin-guard.ts
+++ client/social-network/src/app/guards/admin-guard.ts
@@ -2,7 +2,7 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth';
 
-export const adminGuardGuard: CanActivateFn = () => {
+export const adminGuard: CanActivateFn = () => {
   const router = inject(Router);
   const authService = inject(AuthService);
 
