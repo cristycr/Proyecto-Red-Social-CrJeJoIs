2845e181bb4df27fc320c05cf4712725c40c16f0
Jor <202547325+jor2511@users.noreply.github.com>
2026-03-15 13:37:41 +0100

    Añadir WebSocketManager
    Necesitamos guardar qué usuario tiene qué socket

---
+++ server/SocialNetwork/SocialNetwork/Services/WebSocketManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.WebSockets;
+using System.Collections.Concurrent;
+
+public class WebSocketManager
+{
+    private readonly ConcurrentDictionary<string, WebSocket> _connections = new();
+
+    public void AddConnection(string userId, WebSocket socket)
+    {
+        _connections[userId] = socket;
+    }
+
+    public async Task SendMessage(string userId, string message)
+    {
+        if (_connections.TryGetValue(userId, out var socket))
+        {
+            if (socket.State == WebSocketState.Open)
+            {
+                var bytes = System.Text.Encoding.UTF8.GetBytes(message);
+
+                await socket.SendAsync(
+                    bytes,
+                    WebSocketMessageType.Text,
+                    true,
+                    CancellationToken.None
+                );
+            }
+        }
+    }
+
+    public void RemoveConnection(string userId)
+    {
+        _connections.TryRemove(userId, out _);
+    }
+}
